86edeaeb3487112ca22d8dc22bc865852c2b08a2
Adam Somora <Adam.Somora@4finance.com>
2020-04-25 14:35:52 +0200

    refactor api_client class

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'net/http'
 
-# Communicates to API
+# API handler
 class ApiClient
   def fetch_random_image(attribute)
     url = "https://dog.ceo/api/breed/#{attribute}/images/random"
+    fetch_api_message(url)
+  end
+
+  def fetch_all_images(attribute)
+    url = "https://dog.ceo/api/breed/#{attribute}/images"
+    fetch_api_message(url)
+  end
+
+  def fetch_api_message(url)
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    return 'not found' if JSON.parse(response)['status'] != 'success'
 
     JSON.parse(response)['message']
   end
